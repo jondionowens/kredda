8817d6864fa05d545e210958668188d39b2318a6
Jonathan Owens <jo@jondionowens.com>
2018-10-08 23:33:30 -0700

    Added ProfileView component

---
+++ src/components/ProfileView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+import utils from '../utils/notifications';
+
+class ProfileView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+}
